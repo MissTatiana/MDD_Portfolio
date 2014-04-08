a61de96fef5e125c457af781a7ab5e9ab3affd28
Tatiana Kerick <KerickTatiana@gmail.com>
2014-04-08 13:27:56 -0400

    anuglar added, details working, some style, not floating

--- js/controllers.js
+++ js/controllers.js
@@ -6,3 +6,24 @@ galleryControllers.controller('ListController', ['$scope', '$http', function($sc
 	});
 }]);
 
+galleryControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+	$http.get('js/data.json').success(function(data) {
+		$scope.websites = data;
+		$scope.whichItem = $routeParams.itemId;
+
+		if($routeParams.itemId > 0) {
+			$scope.prevItem = Number($routeParams.itemId)-1;
+		}
+		else {
+			$scope.prevItem = $scope.websites.length-1;
+		}
+
+		if($routeParams.itemId < $scope.websites.length-1) {
+			$scope.nextItem = Number($routeParams.itemId)+1;
+		}
+		else {
+			$scope.nextItem = 0;
+		}
+	});
+}]);
+
---
+++ js/script.js
@@ -0,0 +1,4 @@
+//Need to get the catgories to set up details and get the item
+//id, from there call out the item id and module 2
+//to get to the even or odd and float: left; all odds..
+//Will need to set up a details partials
